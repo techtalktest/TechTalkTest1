11722f048665a8896bc0244543fb4714aeb369d3
andyzhang <andyzhang@adminiMac.local>
2013-05-03 16:03:20 +0800

    0503

---
+++ TeckTalkLogicTests/TeckTalkLogicTests.h
@@ -0,0 +1,13 @@
+//
+//  TeckTalkLogicTests.h
+//  TeckTalkLogicTests
+//
+//  Created by andyzhang on 13-5-3.
+//  Copyright (c) 2013年 andyzhang. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TeckTalkLogicTests : SenTestCase
+
+@end
---
+++ TeckTalkLogicTests/TeckTalkLogicTests.m
@@ -0,0 +1,32 @@
+//
+//  TeckTalkLogicTests.m
+//  TeckTalkLogicTests
+//
+//  Created by andyzhang on 13-5-3.
+//  Copyright (c) 2013年 andyzhang. All rights reserved.
+//
+
+#import "TeckTalkLogicTests.h"
+
+@implementation TeckTalkLogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TeckTalkLogicTests");
+}
+
+@end
