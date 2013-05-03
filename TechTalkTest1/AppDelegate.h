89070da630b75501a4114f42bec435f4c265c30d
andyzhang <andyzhang@adminiMac.local>
2013-05-03 11:52:13 +0800

    tecktalk

---
+++ TechTalkTest1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TechTalkTest1
+//
+//  Created by andyzhang on 13-5-3.
+//  Copyright (c) 2013年 andyzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TechTalkTest1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TechTalkTest1
+//
+//  Created by andyzhang on 13-5-3.
+//  Copyright (c) 2013年 andyzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
